76c02d3d7619c0eafb33317319613bd1a3e2df24
Mauricio Quiñe <mfagotto@gmail.com>
2022-09-24 19:58:16 -0500

    Ultimos Cambios

--- src/main/resources/scripts/base_script.sql
+++ src/main/resources/scripts/base_script.sql
@@ -15,19 +15,11 @@ ALTER TABLE Clientes ADD PRIMARY KEY (id) ;
 ALTER TABLE Clientes MODIFY COLUMN id int auto_increment NOT NULL;
 
 INSERT INTO Clientes (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Pedro','Lopez','DNI70563829','123456','DNI','70563829',1);
-
-INSERT INTO Clientes (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Camila','Paredes','DNI56384920','123456','DNI','56384920',1);
-
-INSERT INTO Clientes (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Andres','Ramirez','DNI8593489','123456','DNI','8593489',1);
-
-INSERT INTO Clientes (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Ruben','Marquez','DNI73920495','123456','DNI','73920495',1);
-
-INSERT INTO Clientes (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Lucia','Alvitez','DNI74725894','123456','DNI','74725894',1);
+VALUES ('Pedro','Lopez','DNI70563829','123456','DNI','70563829',1) ,
+('Camila','Paredes','DNI56384920','123456','DNI','56384920',1),
+('Andres','Ramirez','DNI8593489','123456','DNI','8593489',1),
+('Ruben','Marquez','DNI73920495','123456','DNI','73920495',1),
+('Lucia','Alvitez','DNI74725894','123456','DNI','74725894',1);
 
 select * from Clientes;
 
